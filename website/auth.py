d54ddff15365034af938b36dcdbc62e5d2ed5fb2
jusmccar <justinmccartney1@yahoo.com>
2022-07-08 17:51:49 -0700

    Add signup and login functionality

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from .models import User
 
 auth = Blueprint("auth", __name__)
 
@@ -10,12 +13,25 @@ def signup():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        valid_signup(email, first_name, password1, password2)
+        if valid_signup(email, first_name, password1, password2):
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account created!", category="success")
+            return redirect(url_for("views.home"))
 
     return render_template("signup.html")
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if valid_login(email, password):
+            flash("Logged in!", category="success")
+            return redirect(url_for("views.home"))
+
     return render_template("login.html")
 
 @auth.route("/logout")
@@ -26,8 +42,12 @@ def valid_signup(email, first_name, password1, password2):
     EMAIL_LEN = 4
     FIRST_NAME_LEN = 2
     PASSWORD_LEN = 7
+    user = User.query.filter_by(email=email).first()
 
-    if len(email) < EMAIL_LEN:
+    if user:
+        flash("Account already exists with specified email.", category="error")
+        return False
+    elif len(email) < EMAIL_LEN:
         flash(f"Email must be at least {EMAIL_LEN} characters.", category="error")
         return False
     elif len(first_name) < FIRST_NAME_LEN:
@@ -40,5 +60,17 @@ def valid_signup(email, first_name, password1, password2):
         flash("Passwords must match.", category="error")
         return False
 
-    flash("Account created!", category="success")
     return True
+
+def valid_login(email, password):
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        if check_password_hash(user.password, password):
+            return True
+        else:
+            flash("Incorrect password, try again.", category="error")
+    else:
+        flash("No account exists with specified email.", category="error")
+
+    return False
